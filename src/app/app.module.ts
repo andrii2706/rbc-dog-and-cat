ba2f97b3f8eea033bffe37deaffd7299a66367a4
andriipaslavskyi <andrii.paslavskyi@sombrainc.com>
2022-09-21 10:39:39 +0300

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,44 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NavigationComponent } from './navigation/navigation.component';
+import {HomeComponent} from "./home/component/home/home.component";
+import { NotificationComponent } from './notification/notification/notification.component';
+import {HttpClientModule} from "@angular/common/http";
+import {MatDialogModule} from "@angular/material/dialog";
+import { CardOfAnimalComponent } from './home/component/card-of-animal/card-of-animal.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        FooterComponent,
+        NavigationComponent,
+        NotificationComponent,
+        CardOfAnimalComponent,
+    ],
+    imports: [
+        MatDialogModule,
+        HttpClientModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        FormsModule
+    ],
+    providers: [],
+
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/home/interfaces/HomeInterface.ts
@@ -0,0 +1,5 @@
+export interface HomeInterface {
+  id: number,
+  title: string,
+  description: string,
+}
